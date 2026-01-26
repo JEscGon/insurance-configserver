42d601878280919db1a6267c9b9409092d5ae656
ANONIMO\Jesus <jesusescolargonzalez@gmail.com>
2026-01-26 16:49:03 +0100

    First Commit

---
+++ src/main/java/com/dev/insurance_configserver/InsuranceConfigserverApplication.java
@@ -0,0 +1,13 @@
+package com.dev.insurance_configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceConfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceConfigserverApplication.class, args);
+	}
+
+}
